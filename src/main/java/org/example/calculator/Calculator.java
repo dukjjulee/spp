d7ffd8ae893756227cfeac1dad5132f49eebd192
duckjjulee <nancy0523@naver,com>
2025-07-08 21:17:26 +0900

    잘못작성시 경

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -7,10 +7,42 @@ public static void main (String[] args) {
 
     //두개의 숫자를 입력 = 변수 선언
 
-    System.out.print("숫자 두개를 작성해 주세요.");
+    System.out.print("숫자1: ");
     Scanner input=new Scanner(System.in);
     int num1 = input.nextInt();
+        do {
+            System.out.print("숫자가 너무 작습니다.(양의 정수만 가능):");
+            num1 = input.nextInt();
+        } while (num1 <= 0);
+
+
+    System.out.print("기호: ");
+    char elemen = input.next().charAt(0);
+        do {
+            System.out.print("프로그램에서 사용되는 연산기호가 아닙니다( '+' '-' '*' '/' )");
+            elemen = input.next().charAt(0);
+        } while (elemen == '+' || elemen == '-' || elemen == '*' || elemen == '/');
+
+
+    System.out.println("숫자2");
     int num2 = input.nextInt();
+        do {
+            System.out.print("숫자가 너무 작습니다.(양의 정수만 가능):");
+            num2 = input.nextInt();
+        } while (num2 <= 0);
+
+
+    if (elemen == ('+'))
+    System.out.println( num1 + " + " + num2 + " = " + (num1 + num2) );
+
+    else if (elemen == ('-'))
+        System.out.println( num1 + " - " + num2 + " = " + (num1 - num2) );
+
+    else if (elemen == ('*'))
+        System.out.println( num1 + " * " + num2 + " = " + (num1 * num2) );
+
+    else if (elemen == ('/'))
+        System.out.println( num1 + " / " + num2 + " = " + (num1 / num2) );
 
 
 }
